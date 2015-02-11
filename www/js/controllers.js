ffb15b3a7115c42027ce7bf111fb12fc79c06496
krishavalencia <kakaivalencia_21@yahoo.com>
2015-02-10 17:38:19 -0800

    including time (in progress)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,11 +4,13 @@ angular.module('starter.controllers', [])
 .controller('AccountCtrl', function($scope, $cordovaBarcodeScanner, $http) {
   $scope.insertdatako = function(){
      $cordovaBarcodeScanner.scan().then(function(imageData){
-   var json = imageData.text;
+   var image = imageData.text;
+   var date = [{"Date": new Date}];
+var json = image.concat(date);
  $http.post(
    'https://api.mongolab.com/api/1/databases/testmobile/collections/savehere?apiKey=X8645ILWJXiV_Rmu4gZVn1URuu1WF1Ey', 
-      obj = JSON.parse(json),
-      
+      obj = JSON.parse(json)
+    
   )
   .success(function(){
     alert("yes!!");
